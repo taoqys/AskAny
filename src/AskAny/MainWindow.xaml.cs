d5236179dd476a4292e7c7d71cbcf27725f1dd05
Maybe <taoqys@gmail.com>
2026-09-25 10:25:37 +0800

    fix: import WPF textbox base type

--- src/AskAny/MainWindow.xaml.cs
+++ src/AskAny/MainWindow.xaml.cs
@@ -2,6 +2,7 @@ using System.ComponentModel;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media;
